d400d99c579b39c3e15ab3c888e7091bce943910
chowsh3 <shobir.c@icloud.com>
2020-05-17 04:12:33 +0100

    Updates

---
+++ precache-manifest.9496604c3b96586bf9b06d6386bb1d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aef60cddbd2e66099e30a799390d2e",
+    "url": "/todo_react_application/index.html"
+  },
+  {
+    "revision": "f508e5ff702e1b0d8266",
+    "url": "/todo_react_application/static/css/main.d712f448.chunk.css"
+  },
+  {
+    "revision": "2bb291638c52e9cdc567",
+    "url": "/todo_react_application/static/js/2.2385a930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react_application/static/js/2.2385a930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f508e5ff702e1b0d8266",
+    "url": "/todo_react_application/static/js/main.ace2a421.chunk.js"
+  },
+  {
+    "revision": "5d4c1a4e2e9a73a8502e",
+    "url": "/todo_react_application/static/js/runtime-main.606d0e57.js"
+  }
+]);
